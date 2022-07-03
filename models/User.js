52415c03e25f4e0f9c5c13bf9ebe325e57d7d5ad
Megan Gedde <hoglu070@d.umn.edu>
2022-07-02 19:14:40 -0500

    started models

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: string,
+        required: true,
+        unique: true,
+        //TODO: properly validate per mongoose
+        validate: {
+            validator: () => Promise.resolve(false),
+            message: 'Email validation failed'
+        }
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+  );
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
